f26da4652961571689e9b0fdae65d28d0dba1998
Egor Perevoshchikov <egor_perevoshchikov@mail.ru>
2023-03-15 02:55:43 +0300

    fix: lint msg

--- lesson3/homework/dd/dd.go
+++ lesson3/homework/dd/dd.go
@@ -69,7 +69,7 @@ func process(r io.Reader, w io.Writer, opts *Options) error {
 		if err != nil {
 			return err
 		}
-		readBytes, err = readBlock(r, block, opts.Limit)
+		readBytes, err = readBlock(r, block)
 		readBytesCount := len(readBytes)
 		if err != nil {
 			return err
@@ -107,7 +107,7 @@ func process(r io.Reader, w io.Writer, opts *Options) error {
 	return nil
 }
 
-func readBlock(r io.Reader, block []byte, remainingBytesCount int64) ([]byte, error) {
+func readBlock(r io.Reader, block []byte) ([]byte, error) {
 	readBytesCount, err := r.Read(block)
 
 	if err != nil && err != io.EOF {
@@ -155,20 +155,6 @@ func findStartByteFromBack(block []byte) (byte, int, int) {
 		bytesFromBackCount++
 	}
 	return block[l-bytesFromBackCount], l - bytesFromBackCount, bytesFromBackCount
-}
-
-func runeLen(b byte) int {
-	if b&0b11110000 == 0b11110000 {
-		return 4
-	} else if b&0b11100000 == 0b11100000 {
-		return 3
-	} else if b&0b11000000 == 0b11000000 {
-		return 2
-	} else if b&0b10000000 == 0b10000000 {
-		return 0
-	} else {
-		return 1
-	}
 }
 
 func skipOffset(r io.Reader, opts *Options, block []byte) error {
