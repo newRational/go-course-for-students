68d2f35398b30efa085e17204b0a32ebea03c6cf
Egor Perevoshchikov <egor_perevoshchikov@mail.ru>
2023-04-20 05:29:18 +0300

    fix: lint

--- lesson9/homework/internal/tests/grpc_test.go
+++ lesson9/homework/internal/tests/grpc_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"context"
+	"google.golang.org/grpc/credentials/insecure"
 	"net"
 	"testing"
 	"time"
@@ -49,7 +50,7 @@ func TestGRRPCCreateUser(t *testing.T) {
 		cancel()
 	})
 
-	conn, err := grpc.DialContext(ctx, "", grpc.WithContextDialer(dialer), grpc.WithInsecure())
+	conn, err := grpc.DialContext(ctx, "", grpc.WithContextDialer(dialer), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	assert.NoError(t, err, "grpc.DialContext")
 
 	t.Cleanup(func() {
